98af438b3f4b9016dbb57c6efe4b30ad14de8e04
vitorwlima <vitorwlima13@gmail.com>
2021-08-26 22:36:13 -0300

    add styled componente e estilos iniciais

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,12 @@
-import '../styles/globals.css'
+import GlobalStyles from '../styles/GlobalStyles'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
 }
 export default MyApp
---
+++ src/styles/GlobalStyles.ts
@@ -0,0 +1,17 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+  @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;700&display=swap');
+
+  body {
+    font-family: 'Montserrat', sans-serif;
+    padding: 0;
+    margin: 0;  
+  }
+
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;  
+  }
+`
