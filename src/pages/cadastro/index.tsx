7de34b1c996b353b1014abe7a329326fccd9e4d7
vitorwlima <vitorwlima13@gmail.com>
2021-08-27 20:52:40 -0300

    add estrutura form cadastro

--- src/pages/cadastro/index.tsx
+++ src/pages/cadastro/index.tsx
@@ -5,13 +5,41 @@ import Image from 'next/image'
 import Link from 'next/link'
 
 import { Form } from '@unform/web'
-import { FormHandles } from '@unform/core'
+import { FormHandles, SubmitHandler } from '@unform/core'
+import * as Yup from 'yup'
 
 import * as S from './styles'
 import { Button, Input } from '../../components'
+import { getValidationErrors } from '../../utils'
+
+type IFormData = {
+  name: string
+  email: string
+  password: string
+}
 
 const Register: NextPage = () => {
   const formRef = useRef<FormHandles>(null)
+
+  const handleRegister: SubmitHandler<IFormData> = async formData => {
+    try {
+      formRef.current!.setErrors({})
+
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Insira seu nome'),
+        email: Yup.string().email('Digite um email válido.').required('Insira o seu email.'),
+        password: Yup.string().required('Insira a sua senha.'),
+      })
+
+      await schema.validate(formData, { abortEarly: false })
+    } catch (err) {
+      if (err instanceof Yup.ValidationError) {
+        const errors = getValidationErrors(err)
+        formRef.current?.setErrors(errors)
+      }
+    }
+  }
+
   return (
     <S.Container>
       <Head>
@@ -25,7 +53,7 @@ const Register: NextPage = () => {
       </S.IntroSection>
       <S.RegisterSection>
         <h3>Insira seus dados:</h3>
-        <Form onSubmit={() => {}} ref={formRef}>
+        <Form onSubmit={handleRegister} ref={formRef}>
           <Input name='name' placeholder='Nome' />
           <Input name='email' placeholder='Endereço de e-mail' />
           <Input name='password' placeholder='Senha' type='password' />
