70926390fb9fe99c7822e4cc3587865aac56cff6
vitorwlima <vitorwlima13@gmail.com>
2021-08-27 23:21:31 -0300

    add rota protegida

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
-const api = axios.create({ baseURL: process.env.NEXT_PUBLIC_SERVER })
+const api = axios.create({ baseURL: process.env.NEXT_PUBLIC_SERVER, withCredentials: true })
 
 export default api
