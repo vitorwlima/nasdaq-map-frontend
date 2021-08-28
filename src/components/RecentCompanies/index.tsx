e1c0c569cf012e173305f6f83d2e1a58d69d9040
vitorwlima <vitorwlima13@gmail.com>
2021-08-28 17:42:39 -0300

    adaptando carrossel para diferentes telas

--- src/components/RecentCompanies/index.tsx
+++ src/components/RecentCompanies/index.tsx
@@ -4,10 +4,40 @@ import * as S from './styles'
 import { FaChevronRight, FaChevronLeft } from 'react-icons/fa'
 import StatsIcon from '../../assets/stats.svg'
 import Image from 'next/image'
+import { useEffect } from 'react'
 
 export const RecentCompanies = () => {
   const [currentCompany, setCurrentCompany] = useState(0)
+  const [maxCardsOnScreen, setMaxCardsOnScreen] = useState(2)
   const data = [1, 2, 3, 4, 5, 6, 7]
+
+  useEffect(() => {
+    const onResize = () => {
+      if (window.innerWidth >= 2560) {
+        return setMaxCardsOnScreen(6)
+      }
+      if (window.innerWidth >= 2200) {
+        return setMaxCardsOnScreen(5)
+      }
+      if (window.innerWidth >= 1860) {
+        return setMaxCardsOnScreen(4)
+      }
+      if (window.innerWidth >= 1500) {
+        return setMaxCardsOnScreen(3)
+      }
+      if (window.innerWidth >= 1160) {
+        return setMaxCardsOnScreen(2)
+      }
+      return setMaxCardsOnScreen(1)
+    }
+
+    onResize()
+
+    window.addEventListener('resize', onResize)
+    return () => {
+      window.removeEventListener('resize', onResize)
+    }
+  }, [])
 
   const handlePreviousCompany = () => {
     if (currentCompany === 0) {
@@ -18,7 +48,7 @@ export const RecentCompanies = () => {
   }
 
   const handleNextCompany = () => {
-    if (currentCompany === data.length - 2) {
+    if (currentCompany === data.length - maxCardsOnScreen) {
       return
     }
 
