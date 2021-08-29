3d2d1ccad94021e2122acbd6b66290de29c8b319
vitorwlima <vitorwlima13@gmail.com>
2021-08-28 22:13:21 -0300

    requisicao para pegar empresa mais recente na home

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -3,7 +3,10 @@ import { useRouter } from 'next/dist/client/router'
 import Head from 'next/head'
 import { useEffect } from 'react'
 import { useAppDispatch, useAppSelector } from '../../hooks'
+import { IIntradayPrices } from '../../interfaces'
 import api from '../../services/api'
+import iexApi from '../../services/iexApi'
+import { setQuoteInfo } from '../../state/slices/QuoteSlice'
 import { setUser } from '../../state/slices/UserSlice'
 import { HomeTemplate } from '../../templates'
 
@@ -19,11 +22,29 @@ const Dashboard: NextPage = () => {
           const { data } = await api.get('/refresh-token')
           dispatch(setUser(data))
         } catch {
-          router.push('/login')
+          return router.push('/login')
         }
       }
 
       authenticateByRefresh()
+    } else {
+      const getQuotes = async () => {
+        const quote = user.recentCompanies[0]
+        const quoteResponse = await iexApi.get(`/stable/stock/${quote}/quote?token=${process.env.NEXT_PUBLIC_API_KEY}`)
+        const intradayQuoteResponse = await iexApi.get<IIntradayPrices>(
+          `/stable/stock/${quote}/intraday-prices?token=${process.env.NEXT_PUBLIC_API_KEY}`
+        )
+        if (intradayQuoteResponse.data && quoteResponse.data) {
+          dispatch(
+            setQuoteInfo({
+              quote: quoteResponse.data,
+              intradayQuote: intradayQuoteResponse.data.filter(item => item.close !== null),
+            })
+          )
+        }
+      }
+
+      getQuotes()
     }
   }, [user, dispatch, router])
 
