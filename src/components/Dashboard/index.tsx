82684ee506d9057656de9a88058a5b3211d02cdf
vitorwlima <vitorwlima13@gmail.com>
2021-08-29 19:03:37 -0300

    add loading ao pesquisar pelo input

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -8,6 +8,8 @@ import SearchIcon from '../../assets/search.svg'
 import { Chart, Input, RecentCompanies } from '../'
 import { useRouter } from 'next/dist/client/router'
 import { SubmitHandler } from '@unform/core'
+import { useAppDispatch } from '../../hooks'
+import { setLoaderOpen } from '../../state/slices/LoaderSlice'
 
 type IFormData = {
   company: string
@@ -15,9 +17,11 @@ type IFormData = {
 
 export const Dashboard = () => {
   const router = useRouter()
+  const dispatch = useAppDispatch()
 
   const handleRedirectCompany: SubmitHandler<IFormData> = ({ company }, { reset }) => {
     setTimeout(reset, 1000)
+    dispatch(setLoaderOpen(true))
     return router.push(`/dashboard/${company}`)
   }
 
